a03f0078a602413681624bd9494aeeaad80838ce
Isman Usoh <iamsman@IamSman.local>
2019-09-13 14:00:13 +0700

    Fixed: Index file

---
+++ packages/nest-fireorm/src/index.ts
@@ -0,0 +1,7 @@
+
+export { FireormModuleAsyncOptions, FireormModuleOptions } from "./fireorm-options"
+export { FIREORM_INSTANCT } from './fireorm.constants'
+export { InjectCollectionGroupRepo, InjectCollectionRepo, InjectFirestore } from './fireorm.decorators'
+export { FireormModule } from './fireorm.module'
+export { FireormFeatureOptions, createCollectionProviders } from './fireorm.providers'
+export { getCollectionGroupToken, getCollectionToken } from './fireorm.utils'
