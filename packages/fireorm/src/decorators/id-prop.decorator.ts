3c56e5979d39e213064206ff9834cb47ecc544c0
Benz Kritsada <kritsadaksdtc@gmail.com>
2019-11-12 18:32:16 +0700

    Add manual to id decolator

--- packages/fireorm/src/decorators/id-prop.decorator.ts
+++ packages/fireorm/src/decorators/id-prop.decorator.ts
@@ -1,6 +1,6 @@
 import { getMetadataStorage, IdPropertyMetadataArgs } from "../metadata-storage";
 
-export function IdProp(strategy?: 'uuid/v1' | 'uuid/v4' | 'auto' | (() => string)): Function {
+export function IdProp(strategy?: 'uuid/v1' | 'uuid/v4' | 'auto' | 'manual' | (() => string)): Function {
     return function (object: Object, propertyName: string) {
         getMetadataStorage().ids.push({
             target: object.constructor,
